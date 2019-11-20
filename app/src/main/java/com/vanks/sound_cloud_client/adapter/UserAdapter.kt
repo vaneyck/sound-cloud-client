edaf4bd17e53b11bff9803036f22710097f63a23
Vaneyck Seme Otoki <vaneycksemeotoki@gmail.com>
2019-11-20 09:13:01 +0300

    Show playlist/user name on listing

--- app/src/main/java/com/vanks/sound_cloud_client/adapter/UserAdapter.kt
+++ app/src/main/java/com/vanks/sound_cloud_client/adapter/UserAdapter.kt
@@ -7,9 +7,8 @@ import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.NavController
 import com.vanks.sound_cloud_client.R
-import com.vanks.sound_cloud_client.databinding.SingleAlbumBinding
+import com.vanks.sound_cloud_client.databinding.SingleUserBinding
 import com.vanks.sound_cloud_client.ui.home.HomeFragmentDirections
-import com.vanks.sound_cloud_client.ui.notifications.CollectionFragmentArgs
 import com.vanks.sound_cloud_client.viewmodel.User
 
 class UserAdapter(val navController: NavController) : RecyclerView.Adapter<AlbumViewHolder>() {
@@ -24,9 +23,9 @@ class UserAdapter(val navController: NavController) : RecyclerView.Adapter<Album
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AlbumViewHolder {
         val inflater = LayoutInflater.from(parent.context)
-        val binding = DataBindingUtil.inflate<SingleAlbumBinding>(
+        val binding = DataBindingUtil.inflate<SingleUserBinding>(
             inflater,
-            R.layout.single_album,
+            R.layout.single_user,
             parent,
             false
         )
@@ -51,5 +50,5 @@ class UserAdapter(val navController: NavController) : RecyclerView.Adapter<Album
 
 }
 
-class AlbumViewHolder(val binding: SingleAlbumBinding) :
+class AlbumViewHolder(val binding: SingleUserBinding) :
     RecyclerView.ViewHolder(binding.getRoot())
